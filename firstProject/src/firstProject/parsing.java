8710a5b5dd28bbe2ff8a9520ab3558cfb373e706
Romika Chaudhary <152133936+Romsh1@users.noreply.github.com>
2024-01-11 15:15:27 -0500

    Add files via upload
    - First program in Java about type casting

---
+++ firstProject/src/firstProject/parsing.java
@@ -0,0 +1,16 @@
+package firstProject;
+
+public class parsing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 1029;
+		double doubleNum = (double) num;
+		System.out.println("The value of "+num+ " after converting to double is: "+doubleNum);
+		
+		int convertedInt = (int) doubleNum;
+		System.out.println("The value of "+ doubleNum + " after converting to integer is: "+convertedInt);
+
+	}
+
+}
