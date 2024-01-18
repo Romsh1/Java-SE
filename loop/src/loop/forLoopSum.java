33266d0cfd94c92bdf3aed1b825f57cf2d3325bd
Romika Chaudhary <152133936+Romsh1@users.noreply.github.com>
2024-01-17 22:48:13 -0500

    Add files via upload
    - Calculating total sum by using for loop

---
+++ loop/src/loop/forLoopSum.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class forLoopSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		for (int j = 1; j <= 10; j++) {
+			sum += j;
+		}
+		System.out.println("The sum is " + sum + ".");
+			
+	}
+
+}
